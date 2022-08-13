73f858b9b29cd86e4ef91bb226964632161e4dfc
Mahabub Rahman <45081033+Mahabub618@users.noreply.github.com>
2022-08-13 22:46:42 +0600

    Create F.cpp

---
+++ ICPC Preli 2017/F.cpp
@@ -0,0 +1,52 @@
+/**********************/
+/* Mahabub Rahman     */
+/* Cse 10th           */
+/* Comilla University */
+/**********************/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define       ll                  long long int
+#define       veci                 vector<int>
+#define       vecl                 vector<long long int>
+#define       pb                   push_back
+#define       newline cerr << endl
+#define       pii                  pair<int, int>
+#define       F                    first
+#define       S                    second
+#define       nl                   "\n"
+#define       all(v)               v.begin(), v.end()
+///============ CONSTANT ===============///
+#define mx9   1000000007
+#define mx5   200005
+#define inf   1<<30                                           //infinity value
+#define eps   1e-9
+#define mod   mx9
+///=============== Debugging ============================///
+#ifndef ONLINE_JUDGE
+#define       debug(x) cout << #x << " = " << x << endl
+#define       debug2(x, y)             cout << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define       debug3(x, y, z)          cout << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define       debug4(a, b, c, d)       cout << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#endif
+void init_code()
+{
+    ios::sync_with_stdio(false); cin.tie(0);cout.tie(0);
+    
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+    #endif    
+}
+
+int main()
+{
+    //init_code();
+    ll p, q, y;
+    scanf("%lld %lld %lld", &p, &q, &y);
+    ll tot = 1LL*((5*p)+(q*2))*52*y;
+    printf("%lld\n", tot);
+     
+return 0;
+}
